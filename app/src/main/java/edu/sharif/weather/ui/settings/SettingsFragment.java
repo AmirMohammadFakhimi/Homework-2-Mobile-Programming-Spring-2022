fbd47132364765ef288950272b10af319af58d21
burgereza <srezaghamghaam@gmail.com>
2022-05-17 14:43:02 +0430

    Dark Mode

--- app/src/main/java/edu/sharif/weather/ui/settings/SettingsFragment.java
+++ app/src/main/java/edu/sharif/weather/ui/settings/SettingsFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -15,6 +19,8 @@ import edu.sharif.weather.databinding.FragmentSettingsBinding;
 public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
+    private Switch darkModeSwitch;
+    private Boolean nightMode;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +29,49 @@ public class SettingsFragment extends Fragment {
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        darkModeSwitch =
+                binding.switch1;
+
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_NO);
+                }
+            }
+        });
+
+//        nightMode = false;
+//
+//        darkModeSwitch.setOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view)
+//                    {
+//                        if (!nightMode) {
+//                            AppCompatDelegate
+//                                    .setDefaultNightMode(
+//                                            AppCompatDelegate
+//                                                    .MODE_NIGHT_YES);
+//                            nightMode = true;
+//                        }
+//                        else {
+//                            AppCompatDelegate
+//                                    .setDefaultNightMode(
+//                                            AppCompatDelegate
+//                                                    .MODE_NIGHT_NO);
+//                            nightMode = false;
+//                        }
+//                    }
+//                });
 
         final TextView textView = binding.textDashboard;
         settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
